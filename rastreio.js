49c65dff55b09699424c94835c546f67b1075dc5
CosturaBrasil <140460749+CosturaBrasil@users.noreply.github.com>
2023-07-25 23:40:10 -0300

    Create rastreio.js

---
+++ rastreio.js
@@ -0,0 +1,3 @@
+!function UD_SCOPE(){
+  const html = ''+
+    '<style> .-categories {display: none!important;}li.contact-whatsapp.done {display: none!important;}.holder-footer-logo {display: none!important;}.holder-footer-section.section-2 {display: none!important;}section.done.footer.top-line {display: block!important;}</style>'+
