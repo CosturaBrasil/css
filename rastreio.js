b527818df51896afc0113331dd8d3a829d6cf6c1
CosturaBrasil <140460749+CosturaBrasil@users.noreply.github.com>
2023-07-25 23:47:15 -0300

    Update rastreio.js

--- rastreio.js
+++ rastreio.js
@@ -1,3 +1,4 @@
 !function UD_SCOPE(){
-  const html = ''+
+  const html = 
     '<style> .-categories {display: none!important;}li.contact-whatsapp.done {display: none!important;}.holder-footer-logo {display: none!important;}.holder-footer-section.section-2 {display: none!important;}section.done.footer.top-line {display: block!important;}</style>'+
+}
